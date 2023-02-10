525a61ed7cd1644a93c58a2c8fdd9e0565bbd5c4
Matheus Silva <matheussgsilva@hotmail.com>
2023-02-10 18:52:29 -0300

    Create form submission function

--- app.js
+++ app.js
@@ -5,7 +5,24 @@ import "./src/js/solutions.js";
 
 const hamburger = document.querySelector("#hamburger");
 const navbar = document.querySelector("#navbar");
+const formDemo = document.querySelector("#formDemo");
+const demoModal = document.querySelector("#authentication-modal");
 
 hamburger.addEventListener("click", () => {
     navbar.classList.toggle("navbar-active");
 });
+
+formDemo.addEventListener("submit", event => {
+    event.preventDefault();
+
+    const formData = new FormData(formDemo);
+    const data = Object.fromEntries(formData);
+
+    fetch('https://hook.us1.make.com/3a7l8yrhup3ac2c65ww4bdebgl424r6t', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'applicatio/json'
+        },
+        body: JSON.stringify(data)
+    })
+})
