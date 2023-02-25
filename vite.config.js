26fd1610d2872ab1fa8a6402516233926216b0d8
Matheus Silva <matheussgsilva@hotmail.com>
2023-02-25 20:10:49 -0300

    Change images link and meta tags title and description

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,7 @@ export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                main: resolve(__dirname, "index.html"),
-                contact: resolve(__dirname, "/pages/contact.html"),
-                customers: resolve(__dirname, "/pages/customers.html"),
-                about: resolve(__dirname, "/pages/about.html"),
-                pricing: resolve(__dirname, "/pages/pricing.html"),
-                story: resolve(__dirname, "/pages/story.html"),
-                error: resolve(__dirname, "/pages/404.html"),
-                servererror: resolve(__dirname, "/pages/500.html"),
-                solution: resolve(__dirname, "/pages/solution.html"),
+                main: resolve(__dirname, "index.html")
             },
         },
     },
