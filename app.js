271dd39ddf45c8a2f941def9e4e56c958065d3fc
Matheus Silva <matheussgsilva@hotmail.com>
2023-03-11 18:08:56 -0300

    Change tag manager to app.js file

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ const modalText = document.querySelectorAll(".modal-text");
 const url = new URL(window.location);
 const path = url.search;
 
+window.onload = function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    }(window,document,'script','dataLayer','GTM-TCR5WHF');
+
 
 mockup.addEventListener("click", () => {
   video.classList.remove("hidden");
